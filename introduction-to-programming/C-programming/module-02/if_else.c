c62d51bf3b710043dd74cf3fef59fd01f701cffd
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-07 21:37:21 +0600

    Fix output formatting in if-else statements for clarity

--- introduction-to-programming/C-programming/module-02/if_else.c
+++ introduction-to-programming/C-programming/module-02/if_else.c
@@ -4,8 +4,8 @@ int main()
     int tk;
     scanf("%d", &tk);
     if (tk >= 100)
-    printf("bargar khabo");
+    printf("bargar khabo/\n");
 
     else
-    printf("bargar khabo na");
+    printf("bargar khabo na\n");
 }
