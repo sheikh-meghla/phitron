2781cf8d9ea7379ca6aee46b1ac69b3e0f22e952
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-07 23:44:24 +0600

    Add nested if-else logic for travel destination selection based on user input

---
+++ introduction-to-programming/C-programming/module-02/nested_if.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if (tk >= 5000)
+    {
+        printf("Cox's bazar jabo\n");
+        if (tk >= 10000)
+        {
+            printf("Saint Martin jabo\n");
+        }
+        else
+        {
+            printf("chole asbo\n");
+        }
+    }
+    else
+    {
+        printf("kothao jabo na\n");
+    }
+
+    
+   
+}
